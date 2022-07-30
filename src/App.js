3335fa1136080a4794e7abf7ad775c0b58322730
Nursultan <nursultan@MacBook-Pro-Nursultan.local>
2022-07-30 22:47:42 +0500

    draft template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Button, Form, Input, Row, Col, Layout, Card } from 'antd';
+
+const { Content } = Layout;
 
 function App() {
+  const [form] = Form.useForm();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className='layout'>
+      <Content
+        style={{
+          padding: '0 50px',
+        }}
+      >
+        <Row gutter={24}>
+          <Col span={12} push={6}>
+            <Card>
+              <Form form={form} layout='vertical'>
+                <Row gutter={24} align='bottom'>
+                  <Col span={12}>
+                    <Form.Item label='from 1000 RUR before 99000 RUR'>
+                      <Input placeholder='enter amount' />
+                    </Form.Item>
+                  </Col>
+                  <Col span={12}>
+                    <Form.Item label='Reserve 80152023 KZT'>
+                      <Input placeholder='enter amount' />
+                    </Form.Item>
+                  </Col>
+                </Row>
+                <Row gutter={24}>
+                  <Col span={24}>
+                    <Card title='Details Sberbank'>
+                      <Row gutter={24}>
+                        <Col span={24}>
+                          <Form.Item label='Your card number'>
+                            <Input placeholder='Your card number' />
+                          </Form.Item>
+                        </Col>
+                      </Row>
+                    </Card>
+                  </Col>
+                </Row>
+                <Row gutter={24}>
+                  <Col span={24}>
+                    <Card title='Details Kaspi bank'>
+                      <Row gutter={24}>
+                        <Col span={24}>
+                          <Form.Item label='Your card number'>
+                            <Input placeholder='Your card number' />
+                          </Form.Item>
+                        </Col>
+                      </Row>
+                    </Card>
+                  </Col>
+                </Row>
+                <Row gutter={24}>
+                  <Col span={24}>
+                    <Form.Item label='Email'>
+                      <Input placeholder='email@email.com' />
+                    </Form.Item>
+                  </Col>
+                </Row>
+                <Form.Item>
+                  <Button type='primary'>Submit</Button>
+                </Form.Item>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
   );
 }
 
